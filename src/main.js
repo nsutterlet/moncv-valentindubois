56b5c45fbee598b8c7e148acc0194231f8e0522d
valentindubois <valentin.dubois2@he-arc.ch>
2017-06-27 20:06:22 +0200

    Version initiale avec config jQuery

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.css';
+
+$(document).ready(function () {
+    console.log('it works!');
+});
